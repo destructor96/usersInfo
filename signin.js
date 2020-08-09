e83e7a8c1f1c90c89aa63577dd3db6eae2189c59
destructor96 <mfaheemsharif96@gmail.com>
2020-08-09 19:30:51 +0500

    Login and Signup Page

---
+++ signin.js
@@ -0,0 +1,29 @@
+const signInApiCall = (user) => {
+  var email = document.getElementById("LoginEmail").value;
+  var pass = document.getElementById("LoginPass").value;
+  var user = {
+    username: email,
+    password: pass,
+  };
+  axios
+    .post("https://d0i36c296b.execute-api.us-east-2.amazonaws.com/dev/login", user)
+    .then((response) => {
+      
+      if (response.status == 200) {
+        if (response.data.statusCode == 200) {
+          console.log(`Success: `, response);
+        }
+        else if (response.data.statusCode == 401) {
+          document.getElementById("WrongCreds").innerHTML = response.data.message;
+          document.getElementById("WrongCreds").style.display = "block";
+          document.getElementById("WrongCreds").style.color = "#f54245"
+          
+        }
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("WrongCreds").style.display = "block";
+      return false;
+    });
+};
